459e31d609cabdc5854ff36a63671459bbbc61ab
Malek <sy5964910@gmail.com>
2024-03-01 08:38:39 -0500

    Establishing Workspace Structue

---
+++ Essential_Data_Structures./Stack_example.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+using namespace std;
+
+int main
+{
+    // It's way more effeciant to copy and paste a code that explains the structure form any website eg("GeekForGeeks","stackoverflow"...)
+}
